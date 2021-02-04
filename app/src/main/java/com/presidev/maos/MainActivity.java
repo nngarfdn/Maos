9256e1b4258f2fb4727c47e9873032ee650221a3
nngarfdn <33192900+nngarfdn@users.noreply.github.com>
2021-02-04 03:16:16 +0000

    tes commit

--- app/src/main/java/com/presidev/maos/MainActivity.java
+++ app/src/main/java/com/presidev/maos/MainActivity.java
@@ -3,6 +3,7 @@ package com.presidev.maos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "Commit", Toast.LENGTH_SHORT).show();
     }
 }
