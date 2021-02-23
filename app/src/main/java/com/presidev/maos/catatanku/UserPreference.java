d8401967b52059854de9d74dbbbf7519ce77f7c2
ariefzuhri <ariefzuhri72@gmail.com>
2021-02-23 13:11:57 +0700

    Tambah hapus target, hint target harian

---
+++ app/src/main/java/com/presidev/maos/catatanku/UserPreference.java
@@ -0,0 +1,42 @@
+package com.presidev.maos.catatanku;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import static com.presidev.maos.utils.DateUtils.getCurrentDate;
+
+public class UserPreference {
+    private final SharedPreferences sharedPreferences;
+    private final SharedPreferences.Editor editor;
+
+    private static final String PREFERENCE_NAME = "user_preference";
+    private static final String LAST_UPDATE_PAGES_READ = "last_update_pages_read";
+    private static final String TODAY_PAGES_READ = "today_pages_read";
+
+    @SuppressLint("CommitPrefEdits")
+    public UserPreference(Context context){
+        sharedPreferences = context.getSharedPreferences(PREFERENCE_NAME, Context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+    }
+
+    public void setData(String targetId, String lastUpdatePagesRead, int todayPagesRead){
+        editor.putString(LAST_UPDATE_PAGES_READ +  "_" + targetId, lastUpdatePagesRead);
+        editor.putInt(TODAY_PAGES_READ +  "_" + targetId, todayPagesRead);
+        editor.apply();
+    }
+
+    public void removeData(String targetId){
+        editor.remove(LAST_UPDATE_PAGES_READ +  "_" + targetId);
+        editor.remove(TODAY_PAGES_READ +  "_" + targetId);
+        editor.apply();
+    }
+
+    public String getLastUpdatePagesRead(String targetId){
+        return sharedPreferences.getString(LAST_UPDATE_PAGES_READ +  "_" + targetId, getCurrentDate());
+    }
+
+    public int getTodayPagesRead(String targetId){
+        return sharedPreferences.getInt(TODAY_PAGES_READ +  "_" + targetId, 0);
+    }
+}
--- app/src/main/java/com/presidev/maos/catatanku/target/AddUpdateTargetActivity.java
+++ app/src/main/java/com/presidev/maos/catatanku/target/AddUpdateTargetActivity.java
@@ -1,5 +1,6 @@
 package com.presidev.maos.catatanku.target;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -12,19 +13,25 @@ import android.view.View;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.presidev.maos.R;
+import com.presidev.maos.catatanku.UserPreference;
 import com.presidev.maos.databinding.ActivityAddUpdateTargetBinding;
 
 import static com.presidev.maos.utils.AppUtils.getFixText;
 import static com.presidev.maos.utils.AppUtils.showToast;
 import static com.presidev.maos.utils.Constants.EXTRA_TARGET;
+import static com.presidev.maos.utils.DateUtils.differenceOfDates;
+import static com.presidev.maos.utils.DateUtils.getCurrentDate;
 
 public class AddUpdateTargetActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ActivityAddUpdateTargetBinding binding;
     private Target target;
     private TargetViewModel targetViewModel;
+    private UserPreference preference;
 
     private boolean isUpdate;
+    private int todayPagesRead;
+    private String lastUpdatePagesRead;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,79 +42,99 @@ public class AddUpdateTargetActivity extends AppCompatActivity implements View.O
         setSupportActionBar(binding.toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        preference = new UserPreference(this);
+
         Intent intent = getIntent();
         isUpdate = intent.hasExtra(EXTRA_TARGET);
         if (isUpdate){
             target = intent.getParcelableExtra(EXTRA_TARGET);
             binding.toolbar.setTitle("Edit Target");
+            binding.btnDelete.setVisibility(View.VISIBLE);
 
             binding.edtPagesRead.setText(String.valueOf(target.getPagesRead()));
             binding.edtBookTitle.setText(target.getBookTitle());
             binding.edtTotalPages.setText(String.valueOf(target.getTotalPages()));
             binding.edtDailyPages.setText(String.valueOf(target.getDailyPages()));
+
+            todayPagesRead = preference.getTodayPagesRead(target.getId());
+            lastUpdatePagesRead = preference.getLastUpdatePagesRead(target.getId());
         } else {
             target = new Target();
             binding.toolbar.setTitle("Tambah Target");
+            binding.btnDelete.setVisibility(View.GONE);
 
             binding.edtPagesRead.setText("0");
+
+            todayPagesRead = 0;
+            lastUpdatePagesRead = getCurrentDate();
         }
 
         binding.btnAdd.setOnClickListener(this);
         binding.btnRemove.setOnClickListener(this);
         binding.btnSave.setOnClickListener(this);
+        binding.btnDelete.setOnClickListener(this);
 
         targetViewModel = new ViewModelProvider(this).get(TargetViewModel.class);
 
         setEditTextValidationListener();
+        setHintProgressDaily();
+    }
+
+    private void setHintProgressDaily() {
+        if (differenceOfDates(lastUpdatePagesRead, getCurrentDate()) < 0){
+            // Terakhir update kemarin, reset
+            todayPagesRead = 0;
+            lastUpdatePagesRead = getCurrentDate();
+        }
+
+        int dailyPages = getDailyPagesFromEditText();
+        if (todayPagesRead < dailyPages){
+            binding.tvHintDailyProgress.setText("Kamu belum mencapai target harian (" + todayPagesRead + "/" + dailyPages + ")");
+            binding.tvHintDailyProgress.setTextColor(getResources().getColor(R.color.red));
+        } else {
+            binding.tvHintDailyProgress.setText("Target harian terpenuhi (" + todayPagesRead + "/" + dailyPages + ")");
+            binding.tvHintDailyProgress.setTextColor(getResources().getColor(R.color.green));
+        }
     }
 
     private void setEditTextValidationListener() {
         binding.edtPagesRead.addTextChangedListener(new TextWatcher() {
-            @Override public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+            private int oldPagesRead;
+
+            @Override public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                oldPagesRead = getPagesReadFromEditText();
+            }
 
             @Override public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                int totalPages = 0;
-                if (!getFixText(binding.edtTotalPages).isEmpty())
-                    totalPages = Integer.parseInt(getFixText(binding.edtTotalPages));
-
-                int pagesRead = 0;
-                if (!String.valueOf(charSequence).isEmpty())
-                    pagesRead = Integer.parseInt(String.valueOf(charSequence));
-
+                int totalPages = getTotalPagesFromEditText();
+                int pagesRead = getPagesReadFromEditText();
                 if (pagesRead > totalPages){
+                    todayPagesRead = totalPages;
                     binding.edtPagesRead.setText(String.valueOf(totalPages));
                     showToast(AddUpdateTargetActivity.this,
                             "Progres baca tidak boleh lebih dari jumlah halaman");
+                } else if (pagesRead < totalPages){
+                    todayPagesRead += (getPagesReadFromEditText() - oldPagesRead);
                 }
+                setHintProgressDaily();
             }
 
-            @Override public void afterTextChanged(Editable editable) {
-                if (!getFixText(binding.edtPagesRead).isEmpty()){
-                    if (Integer.parseInt(getFixText(binding.edtPagesRead)) < 0){
-                        binding.edtPagesRead.setText("0");
-                    }
-                }
-            }
+            @Override public void afterTextChanged(Editable editable) {}
         });
 
         binding.edtDailyPages.addTextChangedListener(new TextWatcher() {
             @Override public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
-            @Override public void afterTextChanged(Editable editable) {}
             @Override public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                int totalPages = 0;
-                if (!getFixText(binding.edtTotalPages).isEmpty())
-                    totalPages = Integer.parseInt(getFixText(binding.edtTotalPages));
-
-                int dailyPages = 0;
-                if (!String.valueOf(charSequence).isEmpty())
-                    dailyPages = Integer.parseInt(String.valueOf(charSequence));
-
+                int totalPages = getTotalPagesFromEditText();
+                int dailyPages = getDailyPagesFromEditText();
                 if (dailyPages > totalPages){
                     binding.edtDailyPages.setText(String.valueOf(totalPages));
                     showToast(AddUpdateTargetActivity.this,
                             "Target harian tidak boleh lebih dari jumlah halaman");
                 }
+                setHintProgressDaily();
             }
+            @Override public void afterTextChanged(Editable editable) {}
         });
     }
 
@@ -116,17 +143,13 @@ public class AddUpdateTargetActivity extends AppCompatActivity implements View.O
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.btn_add:
-                int pagesRead1 = 0;
-                if (!getFixText(binding.edtPagesRead).isEmpty())
-                    pagesRead1 = Integer.parseInt(getFixText(binding.edtPagesRead));
-                binding.edtPagesRead.setText(String.valueOf(pagesRead1 + 1));
+                binding.edtPagesRead.setText(String.valueOf(getPagesReadFromEditText() + 1));
                 break;
 
             case R.id.btn_remove:
-                int pagesRead2 = 0;
-                if (!getFixText(binding.edtPagesRead).isEmpty())
-                    pagesRead2 = Integer.parseInt(getFixText(binding.edtPagesRead));
-                binding.edtPagesRead.setText(String.valueOf(pagesRead2 - 1));
+                if (getPagesReadFromEditText() > 0){
+                    binding.edtPagesRead.setText(String.valueOf(getPagesReadFromEditText() - 1));
+                }
                 break;
 
             case R.id.btn_save:
@@ -147,12 +170,11 @@ public class AddUpdateTargetActivity extends AppCompatActivity implements View.O
                 target.setTotalPages(Integer.parseInt(totalPages));
                 target.setDailyPages(Integer.parseInt(dailyPages));
 
-                int pagesRead = 0;
-                if (!getFixText(binding.edtPagesRead).isEmpty())
-                    pagesRead = Integer.parseInt(getFixText(binding.edtPagesRead));
-
+                int pagesRead = getPagesReadFromEditText();
                 target.setPagesRead(pagesRead);
                 target.setProgress(((double) pagesRead/(double) target.getTotalPages()) * 100);
+
+                preference.setData(target.getId(), lastUpdatePagesRead, todayPagesRead);
 
                 if (isUpdate){
                     targetViewModel.update(target);
@@ -164,6 +186,19 @@ public class AddUpdateTargetActivity extends AppCompatActivity implements View.O
 
                 onBackPressed();
                 break;
+
+            case R.id.btn_delete:
+                new AlertDialog.Builder(this)
+                        .setTitle("Hapus target")
+                        .setMessage("Apakah kamu yakin ingin menghapusnya?")
+                        .setNegativeButton("Batal", null)
+                        .setPositiveButton("Ya", (dialogInterface, i) -> {
+                            targetViewModel.delete(target);
+                            preference.removeData(target.getId());
+                            showToast(view.getContext(), "Target berhasil dihapus");
+                            onBackPressed();
+                        }).create().show();
+                break;
         }
     }
 
@@ -171,5 +206,26 @@ public class AddUpdateTargetActivity extends AppCompatActivity implements View.O
     public boolean onSupportNavigateUp() {
         onBackPressed();
         return true;
+    }
+
+    private int getDailyPagesFromEditText(){
+        int dailyPages = 0;
+        if (!getFixText(binding.edtDailyPages).isEmpty())
+            dailyPages = Integer.parseInt(getFixText(binding.edtDailyPages));
+        return dailyPages;
+    }
+
+    private int getTotalPagesFromEditText(){
+        int totalPages = 0;
+        if (!getFixText(binding.edtTotalPages).isEmpty())
+            totalPages = Integer.parseInt(getFixText(binding.edtTotalPages));
+        return totalPages;
+    }
+
+    private int getPagesReadFromEditText(){
+        int pagesRead = 0;
+        if (!getFixText(binding.edtPagesRead).isEmpty())
+            pagesRead = Integer.parseInt(getFixText(binding.edtPagesRead));
+        return pagesRead;
     }
 }
